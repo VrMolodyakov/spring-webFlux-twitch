baa4950d9c1678ac62c1edc14c068135eac62343
Vyacheslav M <vrmolodyakov@mail.ru>
2022-02-26 16:17:09 +0300

    start working

---
+++ twitchBot/src/main/java/com/testProject/twitchBot/TwitchBotApplication.java
@@ -0,0 +1,13 @@
+package com.testProject.twitchBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchBotApplication.class, args);
+	}
+
+}
---
+++ twitchBot/src/test/java/com/testProject/twitchBot/TwitchBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testProject.twitchBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
